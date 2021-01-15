cd509512540c49606dc90cfeae38d40be4814ad4
chrislemus <icrislemus@gmail.com>
2021-01-15 10:55:18 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,15 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
+  # attr_accessor :id, :name, :grade
+  def initialize(attr_hash = {})
+      self.class.column_names.each {|col_name|
+        self.class.attr_accessor col_name.to_sym
+      }
+      attr_hash.each {|k, v| self.send("#{k}=", v)}
+  end
 
 end
